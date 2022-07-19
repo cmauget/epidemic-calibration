cd4a12b22e3119aa11581c6a08c7b5a96504d715
Clement Mauget <clement.mauget@gmail.com>
2022-07-18 22:18:47 -0400

    added SIRX Model + clean up

--- minimze.py
+++ minimze.py
@@ -32,7 +32,7 @@ class SIRModel:
 
         return S, I, R
 
-    def err(self, params, t,  data):
+    def err(self, params, t,  data, y0):
 
 
         beta = params["beta"]
@@ -46,7 +46,7 @@ class SIRModel:
 
         return err
 
-    def fit(self , t, data, guess, N, max_nfev=100000, params=None ):
+    def fit(self , t, data, guess, y0, N, max_nfev=100000, params=None ):
 
         if params is None:
             params = Parameters()
@@ -54,112 +54,59 @@ class SIRModel:
             params.add('gamma',value=guess[1],min=0, max=2, vary=True)
             params.add('N', value=N, vary=False)
 
-        out = minimize(self.err, params, args=(t, data, ),max_nfev=max_nfev)
+        out = minimize(self.err, params, args=(t, data, y0, ),max_nfev=max_nfev)
         return out
 
-#Main function
 
-#-----------------Fitting on simulated data-----------------------#
-data_br = pd.read_csv("data/data_SIR_175_n.csv")
+#-------------------SIRX model----------------------#
+class SIRXModel:
 
+    def __init__(self):
+        pass
 
-data_nr = data_br["Infected"].to_numpy() #fitting on Infected data
+    #SIR EDO
+    def deriv(self, y0, t, N, beta, gamma, kappa, alpha): 
 
+        S, I, R, X = y0
+        #print(y0)
+        
+        dy = np.zeros(4)
+        dy[0] = -beta * S * I / N #dSdt 
+        dy[1] = beta * S * I / N - gamma * I - kappa * I #dIdt 
+        dy[2] = gamma * I + alpha * X#dRdt 
+        dy[3] = kappa * I - alpha * X
+        return dy
 
-N = 3e8 #population
+    def SIRSolve(self, y0, t, N, beta, gamma, kappa):
 
-I0 = 1 #Initial number of infected
-R0 = 0 #Initial number of recovered
-S0 = N - I0 - R0 #initial number of recovered
-y0 = S0, I0, R0 #initial state vector
+        res = odeint(self.deriv, y0, t, args=(N, beta, gamma, kappa)) 
+        S, I, R, X = res.T
 
-n=175 #number of days
-t = np.linspace(0, n, n) #timeseries (\days)
+        return S, I, R, X
 
-#allow quick modification of the number of days to train on
-train_size = 40
-t_train = np.linspace(0, train_size, train_size) #time series for the training
-data = np.resize(data_nr,train_size)
-
-
-
-#Initial guess of our parameters
-beta = 0.3
-gamma = 1./10
-guess = (beta, gamma)
-
-#Creating the SIRModel
-model = SIRModel()
-fitted_model = SIRModel()
-
-#Applying the fit
-res = model.fit(t_train, data, guess, N)
-
-print(res.params)
-
-beta_fit=res.params['beta'].value
-gamma_fit=res.params['gamma'].value
-
-print(beta_fit)
-print(gamma_fit)
-
-fitted_S, fitted_I, fitted_R = fitted_model.SIRSolve(y0, t, N, beta_fit, gamma_fit)
-
-del data_br["Day"]
-plt.axvline(x=train_size,color='gray',linestyle='--', label="End of train dataset")
-plt.plot(t, data_br, '+', label = "Simulated Data")
-plt.plot(t, fitted_S,label="Predicted")
-plt.plot(t, fitted_I, label = "Predicted"),
-plt.plot(t, fitted_R, label = "Predicted")
-plt.legend()
-plt.show()
+    def err(self, params, t,  data, y0):
 
 
-#---------------- Fitted on NYC data---------------------#
-data_nyc_br = pd.read_csv("nyc.csv")
+        beta = params["beta"]
+        gamma = params["gamma"]
+        kappa = params["kappa"]
+        N = params["N"]
 
-data_nyc_nr = data_nyc_br["CASE_COUNT"].to_numpy()#Fitting on Infected data
+        S, I, R  = self.SIRSolve(y0, t, N, beta, gamma, kappa)
+        X = I #TO CHANGE WHEN CHANGING FIT
 
-N = 8000 #population
+        err = X - data
 
-I0 = 1 #Initial number of infected
-R0 = 0 #Initial number of recovered
-S0 = N - I0 - R0 #initial number of recovered
-y0 = S0, I0, R0 #initial state vector
+        return err
 
-n=79 #number of days
-t = np.linspace(0, n, n) #timeseries (\days)
+    def fit(self , t, data, guess, y0, N, max_nfev=100000, params=None ):
 
-#allow quick modification of the number of days to train on
-train_size = 40
-t_train = np.linspace(0, train_size, train_size) #time series for the training
+        if params is None:
+            params = Parameters()
+            params.add('beta',value=guess[0],min=0, max = 10, vary=True)
+            params.add('gamma',value=guess[1],min=0, max=2, vary=True)
+            params.add('kappa',value=guess[1],min=0, max=2, vary=True)
+            params.add('N', value=N, vary=False)
 
-data_nyc = np.resize(data_nyc_nr,train_size)
-
-#Initial guess of our parameters
-beta = 0.3
-gamma = 1./10
-guess = (beta, gamma)
-
-#Creating the SIRModel
-model = SIRModel()
-fitted_model = SIRModel()
-
-#Applying the fit
-res = model.fit(t_train, data_nyc, guess, N)
-
-print(res.params)
-
-beta_fit=res.params['beta'].value
-gamma_fit=res.params['gamma'].value
-
-print("NYC beta fitted "+str(beta_fit))
-print("NYC gamma fitted "+str(gamma_fit))
-
-fitted_S, fitted_I, fitted_R = fitted_model.SIRSolve(y0, t, N, beta_fit, gamma_fit)
-
-plt.axvline(x=train_size,color='gray',linestyle='--', label="End of train dataset")
-plt.plot(t, data_nyc_nr, '+', label = "NYC Data")
-plt.plot(t, fitted_I, label = "Predicted"),
-plt.legend()
-plt.show()
+        out = minimize(self.err, params, args=(t, data, y0, ),max_nfev=max_nfev)
+        return out
