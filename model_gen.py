4bd96970190280a4b8e164668a7f5de4afa3e35f
Clement Mauget <clement.mauget@gmail.com>
2022-07-24 23:40:28 -0400

    updated model_gen for a SIRXVD model

--- model_gen.py
+++ model_gen.py
@@ -8,16 +8,16 @@ import matplotlib.pyplot as plt
 def init(test): #initialise the data
 
     if test:
-        size = 4
-        name_tab = ["Suspected", "Infected", "Recovered", "Quarantined"]
-        cor_tab = np.array([[0,1,0,0], [0,0,1,1], [0,0,0,0], [0, 0, 1, 0]])
-        params = np.array([0.75, 1./10, 1./10, 0.2])
+        size = 5
+        name_tab = ["Suspected", "Infected", "Recovered", "Vacinated", "Death"]
+        cor_tab = np.array([[0,1,0,1,0], [0,0,1,0,1], [0,0,0,0,0], [0,1,0,0,0], [0,0,0,0,0]])
+        params = np.array([0.5, 0.004, 1./10, 1./100, 0])
 
         N = 3e8 #population
         I0 = 1 #Initial number of infected
         R0 = 0 #Initial number of recovered
         S0 = N - I0 - R0 #initial number of recovered
-        y0 = S0, I0, R0, 0
+        y0 = S0, I0, R0, 0, 0
 
     else:
 
@@ -31,19 +31,22 @@ def init(test): #initialise the data
 
 def model_deriv(y , t, N, params, cor_tab, size):
     
-    print(y)
+    #print(y)
     dy = np.zeros(size)
     ind=0
     
-    for j in range(size):
-        for i in range(size):
+    for i in range(size):
+        for j in range(size):
             if cor_tab[i][j]==1: 
-                if i==0:
+                print(ind)
+                if ((i==0) and (j==1)):
                     dy[i]=dy[i]-params[ind]*y[i]*y[1]/N
                     dy[j]=dy[j]+params[ind]*y[i]*y[1]/N
                 else:
                     dy[i]=dy[i]-params[ind]*y[i]
                     dy[j]=dy[j]+params[ind]*y[i]
+
+                print(i,j)
                 ind+=1
     return dy
     
@@ -57,11 +60,9 @@ name_tab, cor_tab, size, params, y0 , N = init(test)
 
 print(params)
 res = odeint(model_deriv, y0, t, args=(N, params, cor_tab, size)) 
-S, I, R, X = res.T
+result = res.T
 
-plt.plot(t, S, 'b', alpha=0.5, lw=2)
-plt.plot(t, I, 'r', alpha=0.5, lw=2)
-plt.plot(t, R, 'g', alpha=0.5, lw=2)
-plt.plot(t, X, 'y', alpha=0.5, lw=2)
-
+for i in range(size):
+    plt.plot(t, result[i,:], alpha=0.5, lw=2, label=name_tab[i])
+plt.legend()
 plt.show()
