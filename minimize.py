931eb9b22a715e01b02b379b4db4529d0e0bc974
Clement Mauget <clement.mauget@gmail.com>
2022-08-06 12:58:11 -0400

    updated minimize and tests for SIRD model

--- minimize.py
+++ minimize.py
@@ -5,7 +5,9 @@ from scipy.integrate import odeint
 from lmfit import minimize, Parameters
 from sklearn.metrics import mean_absolute_error
 
+
 #-------------------SIR model----------------------#
+
 class SIRModel:
 
     def __init__(self):
@@ -30,14 +32,16 @@ class SIRModel:
         
         return N, y0, guess, data_info
 
+
     def init(self, data_info):
-    
+
         data_nr = pd.read_csv("data/"+data_info[0])[data_info[1]].to_numpy()
         data = np.resize(data_nr,int(data_info[2]))
         t_train = np.linspace(0, int(data_info[2]), int(data_info[2])) #time series for the training
         t = np.linspace(0, int(len(data_nr)), int(len(data_nr))) #timeseries (\days)
 
         return data_nr, data, t_train, t
+
 
     #SIR EDO
     def deriv(self, y0, t, N, beta, gamma): 
@@ -84,6 +88,7 @@ class SIRModel:
 
         data_nr, data, t_train, t = self.init(data_info)
 
+        print(methods)
         out = minimize(self.err, params, method=methods, args=(t_train, data, y0, ),max_nfev=max_nfev)
         beta_fit=out.params['beta'].value
         gamma_fit=out.params['gamma'].value
@@ -91,7 +96,8 @@ class SIRModel:
         fitted_curve=self.SIRSolve(y0, t, N, beta_fit, gamma_fit)
         mae = mean_absolute_error(data_nr, fitted_curve[:][1])
 
-        return t, out, data_nr, fitted_parameters, fitted_curve, mae
+        return out, data_nr, fitted_parameters, fitted_curve, mae, t
+
 
 #-------------------SIRD model----------------------#
 class SIRDModel:
@@ -99,8 +105,44 @@ class SIRDModel:
     def __init__(self):
         pass
 
+    def load_config(self, config_name):
+
+        with open("data/"+config_name, "r") as f:
+            data = f.readlines()
+        f.close()
+
+        for i in range(11):
+            data[i]=data[i].rstrip("\n")
+        
+        N = float(data[0])
+        I0 = float(data[1])
+        S0 = N-I0
+        y0 = S0, I0, float(data[2]), float(data[3])
+        data_info = data[4:8]
+
+        guess = [float(x) for x in data[8:11]]
+
+        print(y0)
+        print(data_info)
+        print(guess)
+        
+        return N, y0, guess, data_info
+
+
+    def init(self, data_info):
+
+        data_nr= pd.read_csv("data/"+data_info[0])[data_info[1]].to_numpy()
+        data_i = np.resize(data_nr,int(data_info[3]))
+        data_nr_d= pd.read_csv("data/"+data_info[0])[data_info[2]].to_numpy()
+        data_d = np.resize(data_nr_d,int(data_info[3]))
+        t_train = np.linspace(0, int(data_info[3]), int(data_info[3])) #time series for the training
+        t = np.linspace(0, int(len(data_nr)), int(len(data_nr))) #timeseries (\days)
+
+        return data_nr, data_i, data_d, t_train, t
+
+
     #SIR EDO
-    def deriv(self, y0, t, N, beta, gamma, kappa, alpha): 
+    def deriv(self, y0, t, N, beta, gamma, kappa): 
 
         S, I, R, D = y0
         #print(y0)
@@ -108,40 +150,50 @@ class SIRDModel:
         dy = np.zeros(4)
         dy[0] = -beta * S * I / N #dSdt 
         dy[1] = beta * S * I / N - gamma * I - kappa * I #dIdt 
-        dy[2] = gamma * I + alpha * D #dRdt 
-        dy[3] = kappa * I - alpha * D #dDdt
+        dy[2] = gamma * I #dRdt 
+        dy[3] = kappa * I  #dDdt
         return dy
 
-    def SIRSolve(self, y0, t, N, beta, gamma, kappa, alpha):
+    def SIRSolve(self, y0, t, N, beta, gamma, kappa):
 
-        res = odeint(self.deriv, y0, t, args=(N, beta, gamma, kappa, alpha)) 
+        res = odeint(self.deriv, y0, t, args=(N, beta, gamma, kappa)) 
         S, I, R, D = res.T
 
         return S, I, R, D
 
     def err(self, params, t,  data_i, data_d, y0):
 
+
         beta = params["beta"]
         gamma = params["gamma"]
         kappa = params["kappa"]
-        alpha = params["alpha"]
         N = params["N"]
 
-        S, I, R, D  = self.SIRSolve(y0, t, N, beta, gamma, kappa, alpha)
+        S, I, R, D  = self.SIRSolve(y0, t, N, beta, gamma, kappa)
 
-        err = (I - data_i + D - data_d)/2
+        err = I - data_i + D -data_d
 
         return err
 
-    def fit(self , t, data, guess, y0, N, max_nfev=100000, params=None ):
+    def fit(self , config_name, methods='leastsq', max_nfev=100000):
 
-        if params is None:
-            params = Parameters()
-            params.add('beta',value=guess[0],min=0, max = 10, vary=True)
-            params.add('gamma',value=guess[1],min=0, max=2, vary=True)
-            params.add('kappa',value=guess[2],min=0, max=2, vary=True)
-            params.add('alpha',value=guess[3],min=0, max=2, vary=True)
-            params.add('N', value=N, vary=False)
+        N, y0, guess, data_info = self.load_config(config_name)
 
-        out = minimize(self.err, params, args=(t, data, y0, ),max_nfev=max_nfev)
-        return out
+        params = Parameters()
+        params.add('beta',value=guess[0],min=0, max = 10, vary=True)
+        params.add('gamma',value=guess[1],min=0, max=2, vary=True)
+        params.add('kappa',value=guess[2],min=0, max=2, vary=True)
+        params.add('N', value=N, vary=False)
+
+        data_nr, data_i, data_d, t_train, t = self.init(data_info)
+
+        out = minimize(self.err, params, method=methods, args=(t_train, data_i, data_d, y0, ),max_nfev=max_nfev)
+        
+        beta_fit=out.params['beta'].value
+        gamma_fit=out.params['gamma'].value
+        kappa_fit=out.params['kappa'].value
+        fitted_parameters=([beta_fit, gamma_fit, kappa_fit])
+        fitted_curve=self.SIRSolve(y0, t, N, beta_fit, gamma_fit, kappa_fit)
+        mae = mean_absolute_error(data_nr, fitted_curve[:][1])
+
+        return out, data_nr, fitted_parameters, fitted_curve, mae, t 
--- minimize_test_SIRD.py
+++ minimize_test_SIRD.py
@@ -1,5 +1,5 @@
 from matplotlib.font_manager import json_dump
-from minimize import SIRModel
+from minimize import SIRDModel
 import matplotlib.pyplot as plt, pandas as pd
 
 def disp(train_size, t, fitted_parameters, fit, data_nr, mae, methods):	
@@ -49,17 +49,26 @@ def data_frame(data, start, end, step):
 
 #---------------- Fitted on NYC data ---------------------#
 #Creating the SIRModel
-model = SIRModel()
+model = SIRDModel()
 
 methods=["leastsq",'least_squares','differential_evolution','brute','basinhopping','ampgo','nelder','lbfgsb','powell','cg','cobyla','bfgs','tnc','trust-constr','slsqp','shgo','dual_annealing']
 methods=["leastsq",'least_squares']
 data = {'Starting_Days': [], 'Methods': [], 'Beta': [], 'Gamma': [], 'Mae': []}
 
+"""
 start, end, step = 55, 80, 5
 for i in range(2):
 	for j in range(start,end,step):
 		change_train_size("config.txt", j)
-		out, data_nr, fitted_parameters, fit, mae, t = model.fit("config.txt" , methods[i])
+		t, out, data_nr, fitted_parameters, fit, mae = model.fit("config.txt" , methods[i])
 		disp(j, t, fitted_parameters, fit, data_nr, mae, methods[i])
 		data = data_set(data, j, methods[i], fitted_parameters, mae)
 data_frame(data, start, end, step)
+"""
+
+out, data_nr, fitted_parameters, fit, mae, t = model.fit("config2.txt")
+
+print(fitted_parameters)
+
+
+disp(100, t, fitted_parameters, fit, data_nr, mae, methods[0])
